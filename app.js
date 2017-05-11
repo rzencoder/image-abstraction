4c31b7ebfa5eda9a3b89470005b4ad4af28f9e69
adam <hamer88@hotmail.co.uk>
2017-05-11 20:24:20 +0100

    file structure

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
 
 app.listen(process.env.PORT || 3000, function() {
   console.log('app is now listening');
